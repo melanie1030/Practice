34108491345013e04e1e284b69a107cb01ae99ae
melanie1030 <152469752+melanie1030@users.noreply.github.com>
2024-05-17 12:46:41 +0800

    Update HW1-analyze.py

--- HW1-analyze.py
+++ HW1-analyze.py
@@ -2,5 +2,13 @@ import time
 import streamlit as st
 import numpy as np
 import pandas as pd
-
+#標題
 st.title('我的第一個應用程式')
+#建立表格
+st.write("嘗試創建**表格**：")
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+df
