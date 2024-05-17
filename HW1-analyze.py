e54c8c3b00699985ddef40e50a65051ae10310ee
melanie1030 <152469752+melanie1030@users.noreply.github.com>
2024-05-17 16:10:52 +0800

    Update HW1-analyze.py

--- HW1-analyze.py
+++ HW1-analyze.py
@@ -56,7 +56,10 @@ if uploaded_file is not None:
         elif chart_type == '長條圖':
             st.bar_chart(chart_data.set_index(x_option))
         elif chart_type == '盒鬚圖':
-            st.box_chart(chart_data.set_index(x_option))
+            if len(chart_data) < 5:  # 如果數據點少於5個，無法繪製盒鬚圖
+                st.error("此資料無法顯示盒鬚圖")
+            else:
+                st.box_chart(chart_data.set_index(x_option))
         elif chart_type == '散點圖':
             st.scatter_chart(chart_data, x=x_option, y=y_option)
 
