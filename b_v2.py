05191919eb12a50eb66f08e5b1a7f58403e1b4dc
melanie1030 <152469752+melanie1030@users.noreply.github.com>
2025-01-07 20:02:57 +0800

    Update b_v2.py

--- b_v2.py
+++ b_v2.py
@@ -8,7 +8,7 @@ import os
 import dotenv
 import base64
 import io
-
+from openai import OpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
